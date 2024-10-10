27832a3b05dc9df2bf35035c0c7bb30e51c9406d
Thiago Lopes <tgmontlopes54@outlook.com>
2024-10-10 08:49:24 -0300

    alteração do codigo

--- menu.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const openMenuButton = document.getElementById('openMenuButton');
-    const mobileMenu = document.getElementById('mobileMenu');
-
-    openMenuButton.addEventListener('click', function() {
-        mobileMenu.classList.toggle('hidden');
-    });
-});
